f82c2c3319570d3421a931f8ae374ed0e6c21c54
Shweta Nigam <shwetanigam2106@gmail.com>
2025-06-28 16:38:02 +0530

    updated review controller

--- backend/src/controllers/review.controllers.js
+++ backend/src/controllers/review.controllers.js
@@ -5,16 +5,14 @@ import { db } from "../db/db.js";
 export const addReview = async (req, res) => {
   const { title, message, rating } = req.body;
 
-
-  const {bookId} = req.params
+  const { bookId } = req.params;
   const userId = req.user.id;
 
-    if (!bookId) {
+  if (!bookId) {
     throw new apiError(400, "Book Id is required");
   }
 
-
-  if (!title || !message || !rating ) {
+  if (!title || !message || !rating) {
     throw new apiError(400, "All fields are required");
   }
 
@@ -32,23 +30,24 @@ export const addReview = async (req, res) => {
     const review = await db.review.findFirst({
       where: { id: newReview.id },
       select: {
-        id:true,
+        id: true,
         title: true,
         message: true,
-        rating: true
-      }
+        rating: true,
+      },
     });
 
-    return res.status(200).json(new apiResponse(201, review, "review created successfully"));
+    return res
+      .status(200)
+      .json(new apiResponse(201, review, "review created successfully"));
   } catch (error) {
-    console.error(error)
-    new apiError(500, "Internal error while creating response")
+    console.error(error);
+    new apiError(500, "Internal error while creating response");
   }
 };
 
-
-export const listReview = async(req,res) => {
-    const {bookId}= req.params
+export const listReview = async (req, res) => {
+  const { bookId } = req.params;
 
   if (!bookId) {
     throw new apiError(400, "Book Id is required");
@@ -56,39 +55,50 @@ export const listReview = async(req,res) => {
 
   try {
     const review = await db.review.findMany({
-        where:{bookId:bookId},
-        select: {
+      where: { bookId: bookId },
+      select: {
+        id: true,
         title: true,
         message: true,
-        rating: true
-      }
-    })
-     return res.status(200).json(new apiResponse(200, review, "successfully fetched reviews"));
+        rating: true,
+      },
+    });
+    return res
+      .status(200)
+      .json(new apiResponse(200, review, "successfully fetched reviews"));
   } catch (error) {
-    console.error(error)
-    new apiError(500, "Internal error while fetching reviews")
+    console.error(error);
+    new apiError(500, "Internal error while fetching reviews");
   }
 };
 
-export const deleteReview = async(req,res)=>{
-     const {id} = req.params
-      if (!id) {
+export const deleteReview = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
     throw new apiError(400, "Review Id is required");
   }
 
   try {
-const existingReview = await db.review.findUnique({ where: { id } });
+    const existingReview = await db.review.findUnique({ where: { id } });
 
-if (!existingReview) {
-  throw new apiError(404, "Review not found");
-}
+    if (!existingReview) {
+      throw new apiError(404, "Review not found");
+    }
 
     const review = await db.review.delete({
-        where:{id}
-    })
-     return res.status(200).json(new apiResponse(200, review, "successfully deleted review"));
+      where: { id },
+      select: {
+        id: true,
+        title: true,
+        message: true,
+        rating: true,
+      },
+    });
+    return res
+      .status(200)
+      .json(new apiResponse(200, review, "successfully deleted review"));
   } catch (error) {
-    console.error(error)
-     throw new apiError(500, "Internal error while deleting review")
+    console.error(error);
+    throw new apiError(500, "Internal error while deleting review");
   }
 };
