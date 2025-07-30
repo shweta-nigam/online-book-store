5cbb3bd0a326a942730ba2e991f7c0d9ef69c88d
Shweta Nigam <shwetanigam2106@gmail.com>
2025-07-30 21:52:05 +0530

    fix: add review page

--- frontend/src/routes/ProfileRoutes.jsx
+++ frontend/src/routes/ProfileRoutes.jsx
@@ -5,11 +5,13 @@ import OrderPage from "@/pages/Profile/ProfileOrderPage";
 import ProfilePage from "@/pages/Profile/ProfilePage";
 import ProfileSettingPage from "@/pages/Profile/profileSettingPage";
 import ProtectedRoute from "./ProtectedRoute";
+import ReviewPage from "@/pages/Book/ReviewPage";
 
 const ProfileRoute = (
   <>
     <Route path="/profile" element={<ProfilePage />}>
       <Route path="orders" element={<OrderPage />} />
+      <Route path="reviews" element={<ReviewPage />} />
       <Route
         path="settings"
         element={
