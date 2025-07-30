9f8420ad914e33830704870a0f8494d72a912360
Shweta Nigam <shwetanigam2106@gmail.com>
2025-07-30 19:33:18 +0530

    feat: add vercel.json file

--- frontend/src/routes/authRoutes.jsx
+++ frontend/src/routes/authRoutes.jsx
@@ -1,16 +1,14 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
 import LoginPage from "../pages/Auth/LoginPage";
 import SignupPage from "@/pages/Auth/SignUpPage";
 import VerifyPage from "@/pages/Auth/VerifyPage";
 
 const AuthRoute = (
-    <>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-          <Route path="/verify" element={<VerifyPage />} />
-
-     
-    </>
-)
-export default AuthRoute
+  <>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/signup" element={<SignupPage />} />
+    <Route path="/verify" element={<VerifyPage />} />
+  </>
+);
+export default AuthRoute;
